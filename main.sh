6732d37fb91d5995eb285e442d3f62bae2dd5d72
Hitesh Tarani <tarani.hitesh@gmail.com>
2015-08-27 14:47:23 +0530

    Validation of input file added

--- main.sh
+++ main.sh
@@ -4,9 +4,9 @@ then
     echo "No data file provided as an argument"
 else 
     #if [ -z "`ls|grep $1`" ]
-    if [ ! -r "$1" ]
+    if [ ! -s "$1" ]
     then
-        echo "File Not found or not readable"
+        echo "File Not found or is empty"
     else
         file=$1
         echo "Data File is $file"
